5c5d293fa03fe65ceda7cdbcd7c7a013223f325e
Abdeltif Boujamza <abdeltif.b@gclgroup.com>
2020-08-21 22:50:57 +0100

    - Debug as variable

--- orchest/settings/base.py
+++ orchest/settings/base.py
@@ -14,7 +14,7 @@ import os
 from decouple import config, Csv
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', cast=bool)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
